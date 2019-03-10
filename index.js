1df796cb17cebc6bd0aad2691ea43c64326f62c5
tcbyrd <tcbyrd@github.com>
2019-03-10 12:18:10 -0700

    standard formatting

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 const { Toolkit } = require('actions-toolkit')
 const Stale = require('./lib/stale')
 
-module.exports = tools => {
-  if (!tools) tools = new Toolkit({
-    event: [
-      'issue_comment',
-      'issues',
-      'pull_request',
-      'pull_request_review',
-      'pull_request_review_comment',
-      'repository_dispatch'
-    ]
-  })
-  
+module.exports = async tools => {
+  if (!tools) {
+    tools = new Toolkit({
+      event: [
+        'issue_comment',
+        'issues',
+        'pull_request',
+        'pull_request_review',
+        'pull_request_review_comment',
+        'repository_dispatch'
+      ]
+    })
+  }
+
   tools.log.star(`Received ${tools.context.event}!`)
   tools.log.start('Stale action is booting up!')
   tools.log.pending('Retrieving Stale config from `.github/stale.yml`...')
   const config = tools.config('.github/stale.yml')
-  
+
   if (tools.context.event === 'repository_dispatch') {
     const stale = new Stale(tools, config)
     const type = tools.context.payload.issue ? 'issues' : 'pulls'
@@ -25,42 +27,43 @@ module.exports = tools => {
       tools.log.success('Done with mark and sweep!')
     })
   } else {
-    unmark(tools).then(() => {
+    return unmark(tools).then(() => {
       tools.log.success('Unmarked a thing')
     })
   }
-  
-  async function unmark(tools) {
+
+  async function unmark (tools) {
     const stale = new Stale(tools, config)
-    if (!isBot(tools.context)) {
-      let issue =
-        tools.context.payload.issue || tools.context.payload.pull_request
-      const type = tools.context.payload.issue ? 'issues' : 'pulls'
-  
-      // Some payloads don't include labels
-      if (!issue.labels) {
-        try {
-          issue = (await tools.github.issues.get(tools.context.issue())).data
-        } catch (error) {
-          tools.exit.failure('Issue not found')
-        }
+    if (isBot(tools.context)) {
+      tools.log.info('Sender is a Bot. Doing nothing')
+      return tools.exit.neutral()
+    }
+
+    let issue =
+      tools.context.payload.issue || tools.context.payload.pull_request
+    const type = tools.context.payload.issue ? 'issues' : 'pulls'
+
+    // Some payloads don't include labels
+    if (!issue.labels) {
+      try {
+        issue = (await tools.github.issues.get(tools.context.issue())).data
+      } catch (error) {
+        return tools.exit.failure('Issue not found')
       }
-  
-      const staleLabelAdded =
-        tools.context.payload.action === 'labeled' &&
-        tools.context.payload.label.name === stale.config.staleLabel
-  
-      if (
-        stale.hasStaleLabel(type, issue) &&
-        issue.state !== 'closed' &&
-        !staleLabelAdded
-      ) {
-        stale.unmark(type, issue)
-      }
+    }
+    const staleLabelAdded =
+      tools.context.payload.action === 'labeled' &&
+      tools.context.payload.label.name === stale.config.staleLabel
+    if (
+      stale.hasStaleLabel(type, issue) &&
+      issue.state !== 'closed' &&
+      !staleLabelAdded
+    ) {
+      return stale.unmark(type, issue)
     }
   }
 }
 
-function isBot(context) {
+function isBot (context) {
   return context.payload.sender.type === 'Bot'
 }
