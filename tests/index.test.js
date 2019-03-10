7865049bfbd170244d905b9b2d347b8568d2a2b3
tcbyrd <tcbyrd@github.com>
2019-03-10 12:17:35 -0700

    getting tests wired up

--- tests/index.test.js
+++ tests/index.test.js
@@ -20,22 +20,88 @@ function mockToolkit (event, workspace = 'workspace') {
   Toolkit.prototype.warnForMissingEnvVars = jest.fn()
 
   const tools = new Toolkit()
-  tools.exit.success = jest.fn()
+  tools.github = {
+    issues: {
+      createComment: jest.fn().mockResolvedValue(),
+      removeLabel: jest.fn().mockResolvedValue()
+    }
+  }
+  tools.log.success = jest.fn()
+  tools.log.complete = jest.fn()
   return tools
 }
 
 describe('handle-stale-action', () => {
   beforeEach(() => {
     Object.assign(process.env, {
-      GITHUB_EVENT: 'repository_dispatch',
       GITHUB_WORKSPACE: path.join(__dirname, 'fixtures', 'workspace')
     })
   })
 
-  it('exits success on a successful run', () => {
+  it('exits success on a successful run', async () => {
+    const mockMarkAndSweep = require('../lib/stale').prototype.markAndSweep = jest.fn().mockResolvedValue(true)
     const tools = mockToolkit('repository-dispatch')
+    tools.context.event = 'repository_dispatch'
+
     runAction(tools)
-    expect(tools.exit.success).toHaveBeenCalled()
-    expect(tools.exit.success.mock.calls).toMatchSnapshot()
+
+    await mockMarkAndSweep()
+
+    expect(tools.log.success).toBeCalledWith('Done with mark and sweep!')
+    expect(tools.log.success.mock.calls).toMatchSnapshot()
+  })
+
+  it('unmarks on new issue comments', async () => {
+    const tools = mockToolkit('issue-comment')
+    tools.context.event = 'issue_comment'
+
+    await runAction(tools)
+
+    expect(tools.log.success).toBeCalledWith('Unmarked a thing')
+    expect(tools.log.success.mock.calls).toMatchSnapshot()
+  })
+
+  it('Handles issue payloads without labels', async done => {
+    const tools = mockToolkit('no-issue-labels')
+    tools.context.event = 'issue_comment'
+    tools.github = {
+      issues: {
+        get: jest.fn().mockResolvedValue({
+          data: {
+            labels: [{ name: 'label name' }]
+          }
+        })
+      }
+    }
+    await runAction(tools)
+    expect(tools.github.issues.get).toBeTruthy()
+    await done()
+  })
+
+  it('Exits if unable to find issues with labels', async done => {
+    const tools = mockToolkit('no-issue-labels')
+    tools.context.event = 'issue_comment'
+    tools.github = {
+      issues: {
+        get: jest.fn().mockRejectedValue({
+          message: 'Not found'
+        })
+      }
+    }
+    tools.exit.failure = jest.fn()
+    await runAction(tools)
+    expect(tools.exit.failure).toBeCalledWith('Issue not found')
+    await done()
+  })
+
+  it('Exits early if sender is a bot', async done => {
+    const tools = mockToolkit('bot-sender')
+    tools.context.event = 'issue_comment'
+    tools.exit.neutral = jest.fn()
+
+    await runAction(tools)
+
+    expect(tools.exit.neutral).toBeCalled()
+    await done()
   })
 })
